8b8982b1292b057a63837b9eb10d66f3a1eaa31b
Federico <federicolami@hotmail.com>
2026-05-12 22:31:42 -0300

    Agrego 4 funciones para descripcion de datos

---
+++ herramientas.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+
+def obtener_estadisticas(columna):
+    if (df[columna].dtype == object):
+        datos = df[columna].value_counts().to_string()
+    else:
+        datos = df[columna].describe().to_string()
+    return datos
+
+def comparar_grupos(columna_grupo,columna_valor):
+    datos = df.groupby(columna_grupo)[columna_valor].mean().to_string()
+    return datos
+
+def contar_por_categoria(columna):
+    datos = df.groupby(columna)['Attrition'].value_counts(normalize = True).to_string()
+    return datos
+
+def filtrar_y_analizar(filtro_columna, filtro_valor, columna_analisis):
+    datos = df[df[filtro_columna]==filtro_valor]
+    
+    if (df[columna_analisis].dtype == object):
+        return datos[columna_analisis].value_counts().to_string()
+    else:
+        return datos[columna_analisis].describe().to_string()
+    
